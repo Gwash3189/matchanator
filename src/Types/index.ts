381a9bb88277240f438bc43ece28a749ad157f6f
Adam Beck <adamb3189@gmail.com>
2020-04-28 16:02:12 +1000

    feat: move types to typescript

--- src/Types/index.ts
+++ src/Types/index.ts
@@ -3,7 +3,7 @@ export const string = (s: string) => typeof s === 'string'
 export const number = (n: number) => typeof n === 'number'
 export const array = (a: any) => Array.isArray(a)
 export const object = (o: object) => typeof o === 'object' && !array(o)
-export const emptyArray = (a :Array<any>) => array(a) && a.length === 0
+export const emptyArray = <T>(a :Array<T | any>) => array(a) && a.length === 0
 export const not = (f: Function) => (x: any) => !f(x)
 export const or = <T>(...ts: Array<Function>) => (v: T) => ts.some(t => t(v))
 export const nil = (x: null | undefined) => x === null || x === undefined
